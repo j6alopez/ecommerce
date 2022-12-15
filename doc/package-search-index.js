394a9d89e85c853e7153e7b13d12b540cb8d1253
jose <j6a@lopez.com>
2022-12-15 23:52:45 +0100

    feat:documentation
    Current project documentation

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.ecommerce"},{"l":"com.ecommerce.domain"},{"l":"com.ecommerce.domain.key"},{"l":"com.ecommerce.repository"}];updateSearchResults();
