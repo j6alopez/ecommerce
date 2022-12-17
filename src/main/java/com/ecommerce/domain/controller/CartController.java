040e684b7afcad327744035d1d148a45dde88468
jose <j6a@lopez.com>
2022-12-17 22:10:30 +0100

    feat: CartController

--- src/main/java/com/ecommerce/domain/controller/CartController.java
+++ src/main/java/com/ecommerce/domain/controller/CartController.java
@@ -1,15 +1,28 @@
 package com.ecommerce.domain.controller;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.domain.Cart;
+import com.ecommerce.domain.Product;
+import com.ecommerce.service.CartService;
+
+import jakarta.validation.constraints.Positive;
 
 /**
  * RestController Class for /carts endpoint
@@ -17,9 +30,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0
 */
 @RestController
-@RequestMapping("/carts")
-public class CartController {
+@RequestMapping(value = "/carts", produces = MediaType.APPLICATION_JSON_VALUE)
+public final class CartController {
+	
+	private final CartService cartService;
+	
+	@Autowired
+	public CartController (final CartService cartService) {
 		
+		this.cartService = cartService;
+		
+	}
 	/**
 	 * Returns accepted headers as well as Content-Type
 	*/
@@ -33,32 +54,40 @@ public class CartController {
 							 		.build();
 		
 	}
-	/**
-	 * Returns {@link com.ecommerce.domain.Cart} content 
-	*/	
-	@GetMapping("/{id}")
-	public void getCart(@PathVariable int id){ 
-		
-	}
 
 	/**
 	 * Creates a {@link com.ecommerce.domain.Cart} object
 	*/		
 	
-	@PostMapping	
-	public void createCart(@PathVariable int id){
-					
+	@PostMapping()	
+	public ResponseEntity<?> createCart(){
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+							 .body(cartService.createCart());
+		
 	}
+	
+	/**
+	 * Returns {@link com.ecommerce.domain.Cart} content 
+	*/	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getCart(@PathVariable @Positive Long id){ 
+		
+		return ResponseEntity.ok("Body");
+		
+	}	
 
 	/**
 	 * Adds {@link com.ecommerce.domain.Cart} 
 	 * @param id
 	 * 		  Cart id
-	 * @param cartItemList
-	 * 		  List of items to be added
+	 * @param addItems
+	 * 		  Items to be added
 	*/		
 	@PostMapping("/{id}/add")
-	public void updateCart(@PathVariable int id){
+	public ResponseEntity<?> addItems(@PathVariable @Positive Long id, @RequestBody Set<Product> addItems){
+		
+		return ResponseEntity.ok("Body");
 		
 	}
 
@@ -69,8 +98,12 @@ public class CartController {
 	*/	
 	
 	@DeleteMapping("/{id}")
-	public void deleteCart(@PathVariable int id){
-					
-	}	
-	
+	public ResponseEntity<?> deleteCart(@PathVariable @Positive Long id){
+		
+		return ResponseEntity.of(cartService.deleteCartById(id));
+		
+	}
+
 }
+	
+	
