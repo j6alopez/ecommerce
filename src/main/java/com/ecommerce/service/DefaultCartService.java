b657509a69f6651988c525c0035d1cd23f54f02f
jose <j6a@lopez.com>
2022-12-17 20:49:17 +0100

    feat: DefaultCartServiceTest
    Unit Test for DefaulcCartService

--- src/main/java/com/ecommerce/service/DefaultCartService.java
+++ src/main/java/com/ecommerce/service/DefaultCartService.java
@@ -1,15 +1,13 @@
 package com.ecommerce.service;
 
+import java.util.Optional;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.domain.Cart;
 import com.ecommerce.domain.Product;
 import com.ecommerce.repository.CartRepository;
-
-import lombok.RequiredArgsConstructor;
 /**
  * Defualt {@link com.ecommerce.service.CartService} for the ecommerce
  * @author Jose Antonio Lopez
@@ -41,14 +39,14 @@ public class DefaultCartService implements CartService {
 	 * Remove element when  {@link com.ecommerce.domain.Product#getAmount()} is 0
 	*/	
 	
-	public Set<Product> addProductsToCart(Long id, Set<Product> cartItemList) {
+	public Cart addProductsToCart(Long id, Set<Product> cartItemList) {
 		
 		Cart cart = cartRepository.getReferenceById(id);
-		cart.getCartItemList().addAll(cartItemList);
-		cart.getCartItemList().removeIf(product -> product.getAmount() == 0);
+		cart.getCartItemSet().addAll(cartItemList);
+		cart.getCartItemSet().removeIf(product -> product.getAmount() == 0);
 		cartRepository.save(cart);
 		
-		return cart.getCartItemList();		
+		return cart;		
 		
 	}		
 
@@ -56,13 +54,14 @@ public class DefaultCartService implements CartService {
 	 * Deletes entire {@link com.ecommerce.domain.Cart} 
 	*/		
 	
-	public boolean deleteCart(Cart cart) {
+	public Optional<Cart> deleteCartById(Long id) {
 		
-		cartRepository.delete(cart);
+		Optional<Cart> optional = cartRepository.findById(id);	
 		
-		return cartRepository.findById(cart.getId())
-							 .map(c -> true)
-							 .orElse(false);		
+		optional.ifPresent(action -> cartRepository.deleteById(id));
+		
+		return optional;
+
 	}	
 	
 }
---
+++ src/test/java/com/ecommerce/service/DefaultCartServiceTest.java
@@ -0,0 +1,113 @@
+package com.ecommerce.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.ecommerce.domain.Article;
+import com.ecommerce.domain.Cart;
+import com.ecommerce.domain.Product;
+import com.ecommerce.repository.ArticleRepository;
+import com.ecommerce.repository.CartRepository;
+import com.ecommerce.repository.ProductRepository;
+
+/**
+ * Tests Entity relations
+ * @author Jose Antonio Lopez
+ * @version 1.0
+*/
+
+@DataJpaTest
+@Sql("/test.sql")
+public class DefaultCartServiceTest {
+	
+	@Mock
+	private Cart cart;
+	
+	private final CartRepository cartRepository ;	
+	private final ProductRepository productRepository ;		
+	private final ArticleRepository articleRepository ;		
+	
+	@Autowired
+	public DefaultCartServiceTest(CartRepository cartRepository, ProductRepository productRepository, ArticleRepository articleRepository ) {
+		
+		this.cartRepository    = cartRepository;
+		this.productRepository = productRepository;
+		this.articleRepository = articleRepository;
+		
+	}
+	
+	private AutoCloseable autoCloseAble;
+	
+	@BeforeEach
+	public void setUp() {
+		
+		autoCloseAble = MockitoAnnotations.openMocks(this);
+		//given		
+		cart = new Cart();
+				
+	}
+	
+	@AfterEach
+	public void cleanUp() throws Exception {
+		
+		autoCloseAble.close();
+	}
+
+	
+	@Test
+	public void createCart() {
+		//when
+		cartRepository.save(cart);
+		//then
+		assertThat( (int) cartRepository.count()).isOne();		
+
+	}
+	
+	@Test
+	public void addProductsToCart() {
+		//given		
+		Cart persistedCart = cartRepository.save(cart);
+		
+		List<Article> cartArticleList = articleRepository.findAll(); 
+		Set<Product>  cartItemSet = new HashSet<Product>();
+		cartArticleList.stream()
+					   .forEach(article -> cartItemSet.add(new Product(persistedCart, article, 0)));
+				
+		persistedCart.setCartItemSet(cartItemSet);
+		
+		//when
+		Cart cartWithProducts = cartRepository.save(persistedCart);
+		
+		//then
+		assertThat(cartWithProducts.getCartItemSet().size()).isEqualTo(cartArticleList.size());
+
+	}
+	
+	@Test
+	public void deleteCart() {
+		//given	
+		
+		Cart persistedCart = cartRepository.save(cart);
+		//when
+		
+		cartRepository.deleteById(persistedCart.getId());
+		Optional<Cart> optional =  cartRepository.findById(persistedCart.getId());
+		//then
+		assertThat(optional).isEmpty();
+
+	}	
+	
+}
