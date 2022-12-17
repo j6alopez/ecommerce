0a20958f1f4a79c36a38c92b21d326e1493610af
jose <j6a@lopez.com>
2022-12-17 20:46:23 +0100

    feat: CartService Interface is declared
    This will allow Autowiring a Service on the controller

--- src/main/java/com/ecommerce/service/CartService.java
+++ src/main/java/com/ecommerce/service/CartService.java
@@ -1,9 +1,20 @@
 package com.ecommerce.service;
+
+import java.util.Optional;
+import java.util.Set;
+
+import com.ecommerce.domain.Cart;
+import com.ecommerce.domain.Product;
+
 /**
  * CartService Interface.Allows multiple services for {@link com.ecommerce.domain.Cart}
  * @author JoseAntonio Lopez
  * @version 1.0
 */
 public interface CartService {
-
+	
+	public Cart createCart();
+	public Optional<Cart> deleteCartById(Long id);
+	public Cart addProductsToCart(Long id , Set<Product> cartItemList);
+	
 }
